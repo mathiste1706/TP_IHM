e80cf3408787a2fc59e1a7706310e2b99d31ec67
mathiste1706 <mathilde1706.lalanne@gmail.com>
2025-09-27 17:21:17 +0200

    end

--- MyAgent.py
+++ MyAgent.py
@@ -4,10 +4,13 @@ class MyAgent(IvyServer):
         IvyServer.__init__(self, "IvyServer")
         self.name = name
         self.start('127.0.0.1:2010')
-        self.bind_msg(self.handle_msg, "test (.*)")
+        self.bind_msg(self.handle_msg, "sra5 Text=(.*) Confidence=(.*)")
         print("here")
 
-    def handle_msg(self, agent, numid, arg):
-        print(f"[Agent {self.name}] got {arg}, from {agent}")
-        self.send_msg(f"ppilot5 Say=vous avez dit :{arg}")
+    def handle_msg(self, agent, numid, texte, confidence):
+        #print(f"[Agent {self.name}] got Text={texte} with Confidence={confidence}, from {agent}")
+        if float(confidence.replace(",", "."))>0.50:
+            self.send_msg(f"ppilot5 Say=vous avez dit :{texte}")
+        else:
+            self.send_msg(f"ppilot5 Say=Je n'ai pas compris")
 a= MyAgent("MyAgent")
