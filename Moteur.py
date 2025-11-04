9f4004656f7e147a1de66187d332d8e8e8497a70
mathiste1706 <mathilde1706.lalanne@gmail.com>
2025-11-04 17:57:56 +0100

    dernière séance tp5 TO DO Finaliser Moteur check quelle valeur on a pour localisation= s'abonner à tt les cannaux utiles pr localisation, et le geste remplir les attributs gerer si geste et le vocal ne correspondent pas (prioritise le vocal?)

---
+++ Moteur.py
@@ -0,0 +1,31 @@
+from ivy.ivy import IvyServer
+class Moteur(IvyServer):
+    def __init__(self, name):
+        IvyServer.__init__(self, name)
+        self.start('127.0.0.1:2010')
+        self.bind_msg(self.handle_msg, "sra5 Parsed=action=(.*) where=(.*) form=(.*) color=(.*) localisation=(.*) Confidence=(.*) NP=.*")
+
+        self.name = name
+        self.action = ""
+        self.forme = ""
+        self.couleur = ""
+        self.localisation = []
+
+    def fuisionnable(self):
+        if self.action != "" and self.forme != "" and self.localisation != []:
+            return True
+        return False
+
+    def handle_msg(self, agent, numid, action, where, form, color, localisation, confidence):
+        if float(confidence.replace(",", ".")) > 0.50:
+            self.action = action
+            self.where = where
+            self.forme = form
+            self.couleur = color
+            self.localisation = localisation
+        else:
+            self.send_msg(f"ppilot5 Say=Je n'ai pas compris")
+
+
+Moteur=Moteur("Moteur")
+
