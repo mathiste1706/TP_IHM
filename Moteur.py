2d66b1882de765df9e6965e858a7f6a5b261c0c2
Thibault QUEFFELEC <queffelecthibault.com>
2025-11-19 14:37:55 +0100

    Ajout de bus ivy sur processing et completion de moteur.py

--- Moteur.py
+++ Moteur.py
@@ -4,12 +4,15 @@ class Moteur(IvyServer):
         IvyServer.__init__(self, name)
         self.start('127.0.0.1:2010')
         self.bind_msg(self.handle_msg, "sra5 Parsed=action=(.*) where=(.*) form=(.*) color=(.*) localisation=(.*) Confidence=(.*) NP=.*")
+        self.agent.bind_msg(self.handle_palette_click, r"^Palette Click x=(.*) y=(.*)")
+
 
         self.name = name
         self.action = ""
         self.forme = ""
         self.couleur = ""
         self.localisation = []
+        self.coordonnee = []
 
     def fuisionnable(self):
         if self.action != "" and self.forme != "" and self.localisation != []:
@@ -25,6 +28,17 @@ class Moteur(IvyServer):
             self.localisation = localisation
         else:
             self.send_msg(f"ppilot5 Say=Je n'ai pas compris")
+    
+    def handle_palette_click(self, agent, event):
+        try:
+            x = int(event[0])
+            y = int(event[1])
+            self.coordonnee.append([x, y])
+            print(f"[Palette] Clic reçu : x={x}, y={y}")
+
+        except Exception as e:
+            print("[Palette] Erreur parsing coordonnées :", e)
+
 
 
 Moteur=Moteur("Moteur")
